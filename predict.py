c39a57de7575e0e75b1c4d961b7d025a840cf8ec
Chester Palen-Michel <cpalenmichel@brandeis.edu>
2024-03-11 18:24:44 -0400

    Add predict script for test set with no labels

---
+++ predict.py
@@ -0,0 +1,77 @@
+import os
+from argparse import ArgumentParser
+
+import torch
+import numpy as np
+from datasets import load_dataset
+from transformers import AutoTokenizer
+
+from multiclassbert import BertForMultilabelSequenceClassification
+
+
+def run_eval():
+
+    parser = ArgumentParser()
+    parser.add_argument("model")
+    parser.add_argument("--traindata")
+    parser.add_argument("--evaldata")
+    parser.add_argument("outdir")
+
+    args = parser.parse_args()
+
+    col_names = ["text"]
+    model_ckpt = args.model
+
+    tokenizer = AutoTokenizer.from_pretrained(model_ckpt)
+
+    train_dataset = load_dataset('csv', data_files=[
+        args.traindata,
+    ], delimiter='\t',
+                                 column_names=col_names
+                                 )['train']
+
+    test_dataset = load_dataset('csv', data_files=[
+        args.evaldata,
+    ], delimiter='\t', column_names=col_names)['train']
+
+    def tokenize_and_encode(examples):
+        return tokenizer(examples["text"], truncation=True)
+
+    label_set = set()
+    for example in train_dataset:
+        for lab in example["label"].split(","):
+            label_set.add(lab)
+
+    id2label = {i: label for i, label in enumerate(sorted(label_set))}
+    label2id = {label: idx for idx, label in id2label.items()}
+
+    # test_dataset = test_dataset.map(
+    #     lambda x: {label: (1 if label in x["label"].split(',') else 0) for label in label2id}
+    # )
+
+    # test_dataset = test_dataset.map(
+    #     lambda x: {"labels": [x[c] for c in label2id if c != "text" and c != "label"]})
+
+    test_dataset = test_dataset.map(tokenize_and_encode, batched=True)
+
+    num_labels = len(label2id)
+    model = BertForMultilabelSequenceClassification.from_pretrained(model_ckpt,
+                                                                    num_labels=num_labels)
+    model.to('cuda')
+
+    sigmoid = torch.nn.Sigmoid()
+    with torch.no_grad():
+        with open(os.path.join(args.outdir, 'bcms_segmented_predictions.txt'), 'w', encoding='utf8') as outfile:
+            for ex in test_dataset:
+                output = model(**{'input_ids': torch.tensor([ex['input_ids']]).to('cuda'), 'attention_mask': torch.tensor([ex['attention_mask']]).to('cuda')})
+                logits = output.logits
+                logits.to('cpu')
+                probs = sigmoid(torch.Tensor(output.logits.to('cpu')).flatten())
+                threshed_pred = np.zeros(probs.shape)
+                threshed_pred[np.where(probs >= 0.5)] = 1
+                pred_labels = ",".join([id2label[idx] for idx in range(len(probs)) if threshed_pred[idx] == 1])
+                print(f"{pred_labels}", file=outfile)
+
+
+if __name__ == "__main__":
+    run_eval()

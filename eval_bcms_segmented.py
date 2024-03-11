55c98b89da1770795cde5a69c220d8137c231d45
Chester Palen-Michel <cpalenmichel@brandeis.edu>
2024-03-11 14:54:02 -0400

    Make sure is using gpu

--- eval_bcms_segmented.py
+++ eval_bcms_segmented.py
@@ -60,12 +60,11 @@ def run_eval():
     model.to('cuda')
 
     sigmoid = torch.nn.Sigmoid()
-    sigmoid.to('cuda')
     with torch.no_grad():
         with open(os.path.join(args.outdir, 'bcms_segmented_predictions.txt'), 'w', encoding='utf8') as outfile:
             for ex in test_dataset:
                 output = model(**{'input_ids': torch.tensor([ex['input_ids']]).to('cuda'), 'attention_mask': torch.tensor([ex['attention_mask']]).to('cuda')})
-                probs = sigmoid(torch.Tensor(output.logits).flatten().to('cuda'))
+                probs = sigmoid(torch.Tensor(output.logits).flatten().to('cpu'))
                 threshed_pred = np.zeros(probs.shape)
                 threshed_pred[np.where(probs >= 0.5)] = 1
                 pred_labels = ",".join([id2label[idx] for idx in range(len(probs)) if threshed_pred[idx] == 1])
